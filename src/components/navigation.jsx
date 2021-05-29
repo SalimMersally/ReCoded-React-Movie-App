6ef229a4ccd4f1b2360374e8c8e29b3d6422720d
Salim Al Mersally <salim.almersally@lau.edu>
2021-05-29 14:16:56 +0300

    Version 2 of the project done

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
+import Spin from "./spinner";
 import { Button, Navbar, Nav, Form } from "react-bootstrap";
 
 function Navigation() {
   let [searchInput, setSearchInput] = useState("");
+  let [isHidden, setIsHidden] = useState("none");
+  const changeSpin = (e) => {
+    setIsHidden(() => {
+      if (e.target.value === "") {
+        return "none";
+      } else {
+        return "inline";
+      }
+    });
+  };
 
   return (
     <Navbar className="bg-light p-2">
@@ -12,13 +23,19 @@ function Navigation() {
         <Nav.Link href="#home">Home</Nav.Link>
       </Nav>
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Spin display={isHidden} />
         <Form className="d-flex" inline>
           <Form.Control
             type="search"
             placeholder="Search"
             className="mr-2"
             aria-label="Search"
-            onChange={(e) => setSearchInput(e.target.value)}
+            // onSearch = {()=> changeSpin()}
+            onChange={(e) => {
+              setSearchInput(e.target.value);
+              changeSpin(e);
+              setTimeout(() => setIsHidden("none"), 5000);
+            }}
             onKeyDown={(e) => {
               if (e.key === "Enter") {
                 e.preventDefault();
