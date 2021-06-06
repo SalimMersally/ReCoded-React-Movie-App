a774655bed70cf8fdc4c0ca24bcb645f46352d56
Hazem El Sankari <hazemleb96@gmail.com>
2021-06-06 13:55:54 +0300

    Fixed Routing V4

--- src/components/movieDetails.jsx
+++ src/components/movieDetails.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import Image from 'react-bootstrap/Image'
+import { Link, useParams } from 'react-router-dom'
 
+//Movie Item Details Page
 function MovieDetails(props) {
-  const { id, title, poster_path, release_date, vote_average, overview } =
-    props.movie;
+  const { title, poster_path, release_date, vote_average, overview } =
+    props.movie
   return (
     <div>
+      <Link to='/'>{'<'} back</Link>
       <h2>{title}</h2>
       {poster_path ? (
-        <Image src={"https://image.tmdb.org/t/p/w500" + poster_path} />
+        <Image src={'https://image.tmdb.org/t/p/w500' + poster_path} />
       ) : (
         <p>No Photo</p>
       )}
       <p>{overview}</p>
       <h4>Release Date: {release_date}</h4>
       <h5>IMDB Rating: {vote_average}</h5>
+      <p>
+        <b>overview</b>
+      </p>
+      <span>{overview}</span>
     </div>
-  );
+  )
 }
-export default MovieDetails;
+export default MovieDetails
--- src/components/movieItem.jsx
+++ src/components/movieItem.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 import Image from 'react-bootstrap/Image'
 import Badge from 'react-bootstrap/Badge'
 import Row from 'react-bootstrap/Row'
+import { useHistory } from 'react-router-dom'
+
+// Movie Grid Item
 function MovieItem(props) {
-  const { title, poster_path, release_date, vote_average } = props.movie
+  const {
+    id,
+    title,
+    poster_path,
+    release_date,
+    vote_average,
+    overview,
+    genre_ids,
+  } = props.movie
+  const history = useHistory()
+
+  function handleClick(movie) {
+    history.push('movie/' + movie.title)
+  }
+
   return (
     <Row id='movieItemStyle'>
       <h2>{title}</h2>
@@ -14,6 +31,12 @@ function MovieItem(props) {
       )}
       <h4>Release Date: {release_date}</h4>
       <h5>IMDB Rating: {vote_average}</h5>
+      <button
+        onClick={(e) => handleClick(props.movie)}
+        style={{ background: 'grey', color: 'blue' }}
+      >
+        More Info
+      </button>
 
       {/* {vote_average >= 5 ? (
         <Badge pill variant='success'>
