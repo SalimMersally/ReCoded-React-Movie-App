c6375b1396bc11934cd01792f436d5cca382e56a
Hazem El Sankari <hazemleb96@gmail.com>
2021-06-09 20:35:59 +0300

    Began V6 and Actor Page Component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
 //import components
-import React, { useEffect, useContext } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { AppContext, useMovies } from "./StateProvider";
-import Main from "./components/main/";
-import Navigation from "./components/navigation/";
-import Footer from "./components/footer/";
-import MovieDetails from "./components/main/movieDetails";
+import React, { useEffect, useContext } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { AppContext, useMovies } from './StateProvider'
+import Main from './components/main/'
+import Navigation from './components/navigation/'
+import Footer from './components/footer/'
+import MovieDetails from './components/main/movieDetails'
+import { useLocation } from 'react-router-dom'
 
 //import css
-import "./styles.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import './styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 //variables
-let popularPath = "movie/popular";
+let popularPath = 'movie/popular'
 
 export default function App() {
-  const [state, dispatch] = useContext(AppContext);
-  const { movies } = useMovies(popularPath, "", 0);
+  const [state, dispatch] = useContext(AppContext)
+  const { movies } = useMovies(popularPath, '', 0)
 
   useEffect(() => {
-    dispatch({ type: "SET_MOVIES", value: movies });
-  }, [movies]);
+    dispatch({ type: 'SET_MOVIES', value: movies })
+  }, [movies])
 
   return (
     <Router>
       <Navigation />
       <Switch>
-        <Route path="/" exact>
+        <Route path='/' exact>
           <Main />
         </Route>
-        <Route path="/movie/:id">
+        <Route path='/movie/:id'>
           <MovieDetails />
+        </Route>
+        <Route pathname='/search' search='?query='>
+          {/* <Main pathname: "/search", search: "?query="shoes" /> */}
+          {/* const urlParams = new URLSearchParams(window.location.search);
+const myParam = urlParams.get('myParam');*/}
+          {/* const search = useLocation().search;
+  const name = new URLSearchParams(search).get('name'); */}
         </Route>
       </Switch>
       <Footer />
     </Router>
-  );
+  )
 }
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,8 +1,21 @@
-import React from "react";
-import MovieGrid from "./MovieGrid";
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import MovieGrid from './MovieGrid'
+import { AppContext, useMovies } from '../../StateProvider'
 
-function Main() {
-  return <MovieGrid />;
+const path = 'search/movie'
+
+function Main(prop) {
+  const [state, dispatch] = useContext(AppContext)
+  const movies = useMovies(path, prop.name, 0)
+  const { name } = useParams()
+  console.log(prop.name)
+  if (prop.name) {
+    dispatch({ type: 'SET_GENRE', value: state.genreId })
+    dispatch({ type: 'SET_MOVIES', value: movies })
+  }
+
+  return <MovieGrid />
 }
 
-export default Main;
+export default Main
